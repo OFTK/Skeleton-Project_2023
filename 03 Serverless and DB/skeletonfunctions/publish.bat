dcc52b2c048b645119af237886cddbbd2f14bfd4
Nimrod Kaplan <nimrodxkaplan@gmail.com>
2023-04-24 23:30:20 +0300

    Fixed some errors with rcv_msg, added sendc2dmsg

--- 03 Serverless and DB/skeletonfunctions/publish.bat
+++ 03 Serverless and DB/skeletonfunctions/publish.bat
@@ -1,2 +1,2 @@
 func azure functionapp publish %1 --publish-local-settings
-func azure functionapp logstream %1
+func azure functionapp logstream %1 --browser
--- 03 Serverless and DB/skeletonfunctions/receivemessage/__init__.py
+++ 03 Serverless and DB/skeletonfunctions/receivemessage/__init__.py
@@ -1,12 +1,13 @@
 import os
 import json
+import logging
 import azure.functions as func
 
 from typing import List
 from azure.data.tables import TableClient
 
 
-def main(events: List[func.EventHubEvent]) -> func.HttpResponse:
+def main(events: List[func.EventHubEvent]):
 
     connection_string = os.getenv("AzureWebJobsStorage")
 
@@ -18,11 +19,6 @@ def main(events: List[func.EventHubEvent]) -> func.HttpResponse:
                 new_counter = int(counter_value) + 1
                 entity["value"] = new_counter
                 table.update_entity(entity)
-                return func.HttpResponse(
-                    json.dumps({
-                        'prev-counter':counter_value,
-                        'current-counter':new_counter
-                    }), status_code=200)
-        except:
-            return func.HttpResponse(f"Something went wrong", status_code=500)
+        except Exception as e:
+            logging.error(f"Failed to update table entity with error: {e}")
         
