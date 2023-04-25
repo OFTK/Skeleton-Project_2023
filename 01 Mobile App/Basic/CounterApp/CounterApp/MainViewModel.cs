1e784a697307bda8cf10499c36d8b50ef5b7adc1
ofek <tikotzkyofek@gmail.com>
2023-04-25 19:10:57 +0300

    make function work with functions

--- 01 Mobile App/Basic/CounterApp/CounterApp/MainViewModel.cs
+++ 01 Mobile App/Basic/CounterApp/CounterApp/MainViewModel.cs
@@ -3,12 +3,35 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
 
 namespace CounterApp
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        public HubConnection connection;
+        private readonly string baseUrl = "https://skeletonfunctionapp.azurewebsites.net";
+        private readonly string negotiateUrl = "https://skeletonfunctionapp.azurewebsites.net/api/negotiate";
+        private readonly string getCounterUrl = "https://skeletonfunctionapp.azurewebsites.net/api/getcounter";
+        private readonly string updateCounterUrl = "https://skeletonfunctionapp.azurewebsites.net/api/updatecounter";
+        public HttpClient client;
+
         private int _counter1;
+        class SimpleCounter { public int count = 0; };
+        class SignalRConnection
+        {
+            public string url = "";
+            public string AccessToken = "";
+        };
+
+        private string _displayMessage;
+        public string DisplayMessage {get => _displayMessage; set => SetProperty(ref _displayMessage, value); }
         public int Counter1
         {
             get => _counter1;
@@ -18,7 +41,7 @@ namespace CounterApp
         private int _counter2;
         public int Counter2
         {
-            get => _counter2;
+            get => _counter1;
             set => SetProperty(ref _counter2, value);
         }
 
@@ -28,7 +51,19 @@ namespace CounterApp
 
         public MainViewModel()
         {
+            Counter1 = 0;
+            Counter2 = 0;
+            client = new HttpClient();
             IncrementCounterCommand = new Command<string>(IncrementCounterById);
+            Counter1 = GetOnlineCounter();
+//            HttpResponseMessage resp = client.GetAsync(new Uri(negotiateUrl)).Result;
+//            string negotiateresponse = "";
+//            if (resp.IsSuccessStatusCode)
+//            {
+//                negotiateresponse = resp.Content.ReadAsStringAsync().Result;
+//            }
+            connection = new HubConnectionBuilder().WithUrl(new Uri(baseUrl + "/api")).Build();
+            Task task = Task.Run(async () => await ConnectToSignalr());
         }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -36,12 +71,49 @@ namespace CounterApp
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        private async Task ConnectToSignalr()
+        {
+            connection.On<string>("counterUpdate", (data) =>
+            {
+                Counter1 = GetOnlineCounter();
+            });
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+            await connection.StartAsync();
+        }
+
+        private async Task InvokeSignalr(int updatedcounter)
+        {
+            await connection.InvokeAsync("counterUpdate", updatedcounter);
+            //await Clients.All.SendAsync("counterUpdate", updatedcounter);
+        }
+
+
+        private int GetOnlineCounter()
+        {
+            HttpResponseMessage resp = client.GetAsync(new Uri(getCounterUrl)).Result;
+            if (resp.IsSuccessStatusCode)
+            {
+                string webcounterstring = resp.Content.ReadAsStringAsync().Result;
+                DisplayMessage = webcounterstring;
+                SimpleCounter storagejsoncounter = JsonConvert.DeserializeObject<SimpleCounter>(webcounterstring);
+                int storagecounter = storagejsoncounter.count;
+                return storagecounter;
+            }
+            else { return 0; }
+        }
+
         private void IncrementCounterById(string counterId)
         {
-            if (counterId == "0")
-                Counter1++;
-            else if (counterId == "1")
-                Counter2++;
+
+            int oldcounter = GetOnlineCounter();
+            int newcounter = oldcounter + 1;
+            _ = client.GetAsync(string.Format("{0}?counter={1}", updateCounterUrl, newcounter)).Result;
+            Counter1 = newcounter;
+            Task task = Task.Run(async () => await InvokeSignalr(newcounter));
         }
 
         private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
@@ -52,6 +124,11 @@ namespace CounterApp
             storage = value;
             OnPropertyChanged(propertyName);
             return true;
+        }
+
+
+        private void PerformInitialConnection()
+        {
         }
     }
 }
