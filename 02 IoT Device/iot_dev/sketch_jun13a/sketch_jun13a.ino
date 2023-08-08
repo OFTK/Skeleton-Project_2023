11e053b31b2dd2e5e25bc701df07e4a614b7ae36
Nimrod Kaplan <nimrodxkaplan@gmail.com>
2023-08-08 14:49:19 -0700

    Added http

--- 02 IoT Device/iot_dev/sketch_jun13a/sketch_jun13a.ino
+++ 02 IoT Device/iot_dev/sketch_jun13a/sketch_jun13a.ino
@@ -4,6 +4,7 @@
 #include <BLEUtils.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
+#include <HTTPClient.h>
 
 #include "SparkFunBME280.h"
 
@@ -12,8 +13,12 @@ BME280 mySensor;
 float temp = 0;
 float humidity = 0;
 
-#define WIFI_SSID           "Home"
-#define WIFI_PASS           "097452430"
+bool has_wifi_creds = true;
+
+char* ssid = "Home";
+char* pass = "097452430";
+char* azure_update_sens_url = "http://Azure/update-sensor";
+// TODO : Those three should be updated by the web app, moreover they should be set to null at first
 
 #define SERVICE_UUID        "71933006-db61-4c41-bfaa-d374279efb65"
 #define TEMP_CHAR_UUID      "f96c20eb-05c7-4c31-803b-03428eae9aa2"
@@ -33,9 +38,9 @@ class TempCallbacks: public BLECharacteristicCallbacks {
     void onRead(BLECharacteristic *pCharacteristic) {
       temp = mySensor.readTempC();
       pCharacteristic->setValue(temp);
-      char buff[20] = {0};
-      sprintf(buff, "Read Temp: %.2f\0", temp);
-      Serial.println(buff);
+      // char buff[20] = {0};
+      // sprintf(buff, "Read Temp: %.2f\0", temp);
+      // Serial.println(buff); (DEBUG)
     }
 };
 
@@ -43,28 +48,15 @@ class HumdCallbacks: public BLECharacteristicCallbacks {
     void onRead(BLECharacteristic *pCharacteristic) {
       humidity = mySensor.readFloatHumidity();
       pCharacteristic->setValue(humidity);
-      char buff[30] = {0};
-      sprintf(buff, "Read Humidity: %.2f\0", humidity);
-      Serial.println(buff);
+      // char buff[30] = {0};
+      // sprintf(buff, "Read Humidity: %.2f\0", humidity);
+      // Serial.println(buff); (DEBUG)
     }
 };
 
 void setup()
 {
   Serial.begin(115200);
-
-  // Wifi setup
-  Serial.println("Connecting...");
-
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  
-  Serial.println("WiFi connected");
-
 
   // BLE setup
   BLEDevice::init("TINOKIBLE");
@@ -108,6 +100,35 @@ void setup()
 
 void loop()
 {
-  
-  delay(50);
+  if (has_wifi_creds) { // If got wifi_creds, trying to publish data via wifi
+    if (WiFi.status() != WL_CONNECTED) { // Trying to connect
+      WiFi.begin(ssid, pass);
+    } else { // If connected, publishing data
+      WiFiClient client;
+      HTTPClient http;
+      char sens_data_str[100] = {'\0'};
+
+       http.begin(client, azure_update_sens_url);
+       
+       // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      // Data to send with HTTP POST
+      sprintf(sens_data_str, 
+              "device_id=%s&temp=%.2f&humd=%.2f\0", 
+              SERVICE_UUID, 
+              mySensor.readFloatHumidity(), 
+              mySensor.readTempC());           
+
+      // Send HTTP POST request
+      /*int httpResponseCode =*/ http.POST(sens_data_str);
+
+      http.end();
+    }
+  }
+
+  delay(60); // TODO : When everything works, run this every half a minute
 }
