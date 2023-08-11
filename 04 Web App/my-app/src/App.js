38a1e3256d5d56b8fa74626cbe5167a5ae3dfba9
nitzankiosso <nitzankiosso@mail.tau.ac.il>
2023-08-11 18:58:16 +0300

    accesing getfamilystatus function app

--- 04 Web App/my-app/src/App.js
+++ 04 Web App/my-app/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [familyStatus, setFamilyStatus] = useState([]);
+  const familyName = 'family'; // Replace with family name
+
+  useEffect(() => {
+    // Fetch family status data from Azure Function App getfamilystatus
+    axios.get(`https://ilovemybaby.azurewebsites.net/api/getfamilystatus?family=${familyName}`)
+      .then(response => {
+        setFamilyStatus(response.data.status);
+      })
+      .catch(error => {
+        console.error('Error fetching family status:', error);
+      });
+  }, [familyName]);
+
+  console.log('Family Status:', familyStatus);
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1> Family Status for {familyName} family </h1>
+      <ul>
+        {familyStatus.map(status => (
+          <li key={status.babyid}>
+            <p>Baby Name: {status.babyname}</p>
+            <p>Last Update: {status.lastupdate}</p>
+            <p>Location: ({status.latitude}, {status.longtitude})</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
